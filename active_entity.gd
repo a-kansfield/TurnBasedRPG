06302bf3caf912c83fe29c6ed4d59c9f391e1447
Amber <akansf1@gmail.com>
2025-07-15 21:25:12 -0500

    Current entity turn now appears onscreen.

---
+++ active_entity.gd
@@ -0,0 +1,14 @@
+extends Label
+
+var formatString = "%s's Turn"
+var BATTLE
+func _init():
+	SignalBus.playerTurn.connect(setTurnText)
+	SignalBus.enemyTurn.connect(setTurnText)
+	pass
+	
+func setTurnText(entity : Variant, activeEntities: Array):
+	self.text = formatString % entity.get_child(Globals.enemyChildren.STATS).eName
+	
+	
+	pass
